36224d551052fab3d7e2352c2678b1903312299f
hitomi.sasaki <tommy110327@yahoo.co.jp>
2021-01-09 07:37:30 +0000

    follow unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -36,4 +36,53 @@ class UsersController extends Controller
             'microposts' => $microposts,
         ]);
     }
+        
+    /**
+     * ユーザのフォロー一覧ページを表示するアクション
+     * 
+     * @param $id ユーザのid
+     * @return \illuminate\Http\Resposne
+     */
+    public function followings($id)
+    {
+        //IDの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+        
+        //関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+        
+        //ユーザのフォロー一覧を取得
+        $followings = $user->followings()->paginate(10);
+        
+        //フォロー一覧ビューでそれらを表示
+        return view('users.followings', [
+            'user' => $user,
+            'users' => $followings,
+        ]);
+    }
+   
+    /**
+     * ユーザのフォロワー一覧ページを表示するアクション
+     * 
+     * @param $id ユーザのid
+     * @return \illuminate\Http\Resposne
+     */
+    public function followers($id)
+    {
+        //IDの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+        
+        //関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+        
+        //ユーザのフォロー一覧を取得
+        $followers = $user->followers()->paginate(10);
+        
+        //フォロー一覧ビューでそれらを表示
+        return view('users.followers', [
+            'user' => $user,
+            'users' => $followers,
+        ]);
+    }
+        
 }
